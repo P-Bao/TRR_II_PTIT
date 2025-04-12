b5af2e4f86341378fd717b29feaa3833cfdbc1b1
Phan Việt Bảo <145033303+P-Bao@users.noreply.github.com>
2025-04-12 17:10:37 +0700

    Create TRR1004.cpp

---
+++ TRR1004.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int n;
+int graph[105][105];
+
+void solveType1(){
+    for(int i = 1; i <= n; ++i)
+        cout << accumulate(graph[i] + 1, graph[i] + n + 1, 0) << " ";
+}
+
+void solveType2(){
+    cout << n << endl;
+
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= n; ++j) cout << graph[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void solve(){
+    int type, m;
+    cin >> type >> n >> m;
+
+    int u, v;
+    for(int i = 1; i <= m; ++i){
+        cin >> u >> v;
+        graph[u][v] = graph[v][u] = 1;
+    }
+
+    if(type == 1) solveType1();
+    else solveType2();
+}
+
+int main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+    // cout.tie(0);
+
+    freopen("DT.INP", "r", stdin);
+    freopen("DT.OUT", "w", stdout);
+
+    solve();
+
+    //cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms";
+    return 0;
+}
